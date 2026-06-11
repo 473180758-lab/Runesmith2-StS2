9e1ee460e338a725e4805636b983889d1f4263ba
Blizzarre <krid.saetia@gmail.com>
2026-06-12 00:25:13 +0700

    fix protected access

--- Runesmith2Code/Patches/NCardPlayPatches.cs
+++ Runesmith2Code/Patches/NCardPlayPatches.cs
@@ -1,6 +1,8 @@
 #region
 
+using System.Reflection;
 using HarmonyLib;
+using MegaCrit.Sts2.Core.Models;
 using MegaCrit.Sts2.Core.Nodes.Combat;
 using Runesmith2.Runesmith2Code.Cards;
 using Runesmith2.Runesmith2Code.Field;
@@ -9,14 +11,17 @@ using Runesmith2.Runesmith2Code.Field;
 
 namespace Runesmith2.Runesmith2Code.Patches;
 
-[HarmonyPatch(typeof(NCardPlay), nameof(NCardPlay.TryShowEvokingOrbs))]
+[HarmonyPatch(typeof(NCardPlay), "TryShowEvokingOrbs")]
 internal static class NCardPlayTryShowEvokingOrbsPatch
 {
+    private static readonly PropertyInfo CardOwnerNodeProp = AccessTools.Property(typeof(NCardPlay), "CardOwnerNode");
+    private static readonly PropertyInfo CardProp = AccessTools.Property(typeof(NCardPlay), "Card");
+
     [HarmonyPostfix]
     private static void Postfix(NCardPlay __instance)
     {
-        var owner = __instance.CardOwnerNode;
-        var card = __instance.Card;
+        var owner = (NCreature?) CardOwnerNodeProp.GetValue(__instance);
+        var card = (CardModel?) CardProp.GetValue(__instance);
         if (card == null || owner == null) return;
         if (card is not Runesmith2Card runesmithCard) return;
         var runeManager = RunesmithNode.NRuneManager[owner];
@@ -27,10 +32,12 @@ internal static class NCardPlayTryShowEvokingOrbsPatch
 [HarmonyPatch(typeof(NCardPlay), nameof(NCardPlay.HideEvokingOrbs))]
 internal static class NCardPlayHideEvokingOrbsPatch
 {
+    private static readonly PropertyInfo CardOwnerNodeProp = AccessTools.Property(typeof(NCardPlay), "CardOwnerNode");
+    
     [HarmonyPostfix]
     private static void Postfix(NCardPlay __instance)
     {
-        var owner = __instance.CardOwnerNode;
+        var owner = (NCreature?) CardOwnerNodeProp.GetValue(__instance);
         if (owner == null) return;
         var runeManager = RunesmithNode.NRuneManager[owner];
         runeManager?.UpdateVisuals(RuneBreakType.None);
